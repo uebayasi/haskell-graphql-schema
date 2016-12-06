41d11aa582c1256df9b2ce0d69f8d4f480d7f483
Masao Uebayashi <uebayasi@tombiinc.com>
2016-12-06 16:14:22 +0900

    Typo + refactor

--- src/Data/GraphQL/XXX/Schema/Pretty.hs
+++ src/Data/GraphQL/XXX/Schema/Pretty.hs
@@ -76,15 +76,17 @@ prettyFields :: GraphQLFields -> Doc
 prettyFields fs = vcat (map prettyField fs)
 
 prettyField :: GraphQLField -> Doc
-prettyField (GraphQLField (GraphQLFieldName f) as t b) =
-    nest 2 (text f <> prettyArguments as <> colon <+> prettyType t <> exclamation)
-        where
-            exclamation = if b then char '!' else empty
+prettyField (GraphQLField (GraphQLFieldName f) as t b) = nest 2 field
+    where
+        field = text f <> prettyArguments as <> colon <+> prettyType t <> exclamation
+        exclamation = if b then char '!' else empty
 
 prettyArguments :: GraphQLArguments -> Doc
 prettyArguments args = case args of
     [] -> empty
-    (a:as) -> lparen <> prettyArgument a <> vcat (map ((comma <+>) . prettyArgument) as) <> rparen
+    (a:as) -> lparen <> prettyArgument a <> vcat restArgs <> rparen
+        where
+            restArgs = map ((comma <+>) . prettyArgument) as
 
 prettyArgument :: GraphQLArgument -> Doc
 prettyArgument (GraphQLArgument (GraphQLFieldName n) t) = text n <> colon <+> prettyType t
@@ -92,7 +94,7 @@ prettyArgument (GraphQLArgument (GraphQLFieldName n) t) = text n <> colon <+> pr
 prettyType :: GraphQLType -> Doc
 prettyType t = case t of
     GraphQLBoolean                       -> text "Boolean"
-    GraphQLFloat                         -> text "Boolean"
+    GraphQLFloat                         -> text "Float"
     GraphQLList t'                       -> lbrack <> prettyType t' <> rbrack
     GraphQLID                            -> text "ID"
     GraphQLInt                           -> text "Int"
