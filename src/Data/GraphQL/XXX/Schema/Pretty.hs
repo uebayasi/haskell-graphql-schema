091135396f027c0ee98040ed14c1c1db3f605238
Masao Uebayashi <uebayasi@tombiinc.com>
2016-12-07 00:52:05 +0900

    Indent

--- src/Data/GraphQL/XXX/Schema/Pretty.hs
+++ src/Data/GraphQL/XXX/Schema/Pretty.hs
@@ -14,41 +14,41 @@ graphQLPretty statements = render $ vcat (map pretty statements)
 {- GraphQLStatement -}
 
 instance Pretty GraphQLStatement where
-  pretty (EnumDefinition (GraphQLTypeName t) ns)
-    =  text "enum" <+> text t <+> lbrace
-    $$ vcat (map oneName ns)
-    $$ rbrace
-        where
-            oneName (GraphQLEnumName e) = nest 2 (text e)
+    pretty (EnumDefinition (GraphQLTypeName t) ns)
+        =  text "enum" <+> text t <+> lbrace
+        $$ vcat (map oneName ns)
+        $$ rbrace
+            where
+                oneName (GraphQLEnumName e) = nest 2 (text e)
 
-  pretty (InputDefinition (GraphQLTypeName t) fs)
-    =  text "input" <+> text t <+> lbrace
-    $$ prettyFields fs
-    $$ rbrace
+    pretty (InputDefinition (GraphQLTypeName t) fs)
+        =  text "input" <+> text t <+> lbrace
+        $$ prettyFields fs
+        $$ rbrace
 
-  pretty (InterfaceDefinition (GraphQLTypeName t) fs)
-    =  text "interface" <+> text t <+> lbrace
-    $$ prettyFields fs
-    $$ rbrace
+    pretty (InterfaceDefinition (GraphQLTypeName t) fs)
+        =  text "interface" <+> text t <+> lbrace
+        $$ prettyFields fs
+        $$ rbrace
 
-  pretty (ObjectDefinition (GraphQLTypeName t) i fs)
-    =  text "type" <+> text t <+> implements <+> lbrace
-    $$ prettyFields fs
-    $$ rbrace
-        where
-            implements = case i of
-                Nothing                    -> empty
-                (Just (GraphQLTypeName t)) -> text "implements" <+> text t
+    pretty (ObjectDefinition (GraphQLTypeName t) i fs)
+        =  text "type" <+> text t <+> intf <+> lbrace
+        $$ prettyFields fs
+        $$ rbrace
+            where
+                intf = case i of
+                    Nothing                    -> empty
+                    (Just (GraphQLTypeName t)) -> text "implements" <+> text t
 
-  pretty (ScalarDefinition (GraphQLTypeName t))
-    =   text "scalar"
-    <+> text t
+    pretty (ScalarDefinition (GraphQLTypeName t))
+        =   text "scalar"
+        <+> text t
 
-  pretty (UnionDefinition (GraphQLTypeName t) (GraphQLTypeName n:ns))
-    = text "union" <+> text t <+> char '=' <+> text n <+> hcat (restNames ns)
-        where
-            restNames = map ((char '|' <+>) . oneName)
-            oneName (GraphQLTypeName t) = text t
+    pretty (UnionDefinition (GraphQLTypeName t) (GraphQLTypeName n:ns))
+        = text "union" <+> text t <+> char '=' <+> text n <+> hcat (restNames ns)
+            where
+                restNames = map ((char '|' <+>) . oneName)
+                oneName (GraphQLTypeName t) = text t
 
 {- Other types -}
 
@@ -56,10 +56,10 @@ prettyFields :: GraphQLFields -> Doc
 prettyFields fs = vcat (map pretty fs)
 
 instance Pretty GraphQLField where
-  pretty (GraphQLField (GraphQLFieldName f) as t b) = nest 2 field
-    where
-        field = text f <> prettyArguments as <> colon <+> pretty t <> exclamation
-        exclamation = if b then char '!' else empty
+    pretty (GraphQLField (GraphQLFieldName f) as t b) = nest 2 field
+        where
+            field = text f <> prettyArguments as <> colon <+> pretty t <> exclamation
+            exclamation = if b then char '!' else empty
 
 prettyArguments :: GraphQLArguments -> Doc
 prettyArguments args = case args of
